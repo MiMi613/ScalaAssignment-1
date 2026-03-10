8ce58698f865d7a00ed1db492de23f243b213a2c
Fanyuanhang Zhang <f.zhang-17@student.tudelft.nl>
2026-03-10 03:12:18 +0100

    converted to cli

---
+++ Scala Task 1/src/test/scala/Test.scala
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions._
+
+class Test extends scala.annotation.Annotation {
+
+  @Test
+  def testState_1(): Unit = {
+    val result = Main.runWithStartState(3729, 0, 0, "0,1,5,4,3,0")
+    assertEquals("0, 4, 2, 1, 4, 2, 5, 6, 7, 3, 1, 0", result)
+  }
+
+  @Test
+  def testState_2(): Unit = {
+    val result = Main.runWithStartState(8642024, 0, 0, "0,1,5,4,3,0")
+    assertEquals("5, 7, 6, 5, 7, 0, 4, 0", result)
+  }
+
+}
